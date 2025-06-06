ecded5bca0ef3dfe4a89dda2f30f37ed6a8e750d
stealth <satosi17@naver.com>
2025-06-06 19:30:12 +0900

    테스트 위해서 주석만 달았음

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -14,7 +14,7 @@ router = APIRouter(
     prefix="/api/question",
 )
 
-
+# 질문 목록 url
 @router.get("/list", response_model=question_schema.QuestionList)
 def question_list(db: Session = Depends(get_db), page: int = 0, size: int = 10):
     total, _question_list = question_crud.get_question_list(
